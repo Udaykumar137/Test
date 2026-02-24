fb8f3b8e58050402a1dbb7b309f058ece951be01
Uday kumar <kothak.ug20.ece@nitp.ac.in>
2026-02-24 04:15:09 +0000

    sampkeCode

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Example: Simple calculation
+        int number1 = 10;
+        int number2 = 20;
+        int sum = number1 + number2;
+        
+        System.out.println("Sum: " + sum);
+    }
+}
